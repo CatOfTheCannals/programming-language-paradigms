ef46187455b6af4456b40c05001df5c085a7c1f3
Fede <federico-rosende@hotmail.com>
2021-06-22 16:21:32 -0300

    P7) 13).

--- practicas/prolog/p7.pl
+++ practicas/prolog/p7.pl
@@ -299,4 +299,192 @@ iesimo(I, L, X) :- append(P, _, L), length(P, I), last(P, X).
 
 % Ejercicio 8
 
+% i)
 
+% interseccion(L1, L2, L3) :- append(P, S, L1), last(P, E), not(member(E, S)), member(E, L2), append([E], [], L3). PREGUNTA: se puede hacer en un solo caso?
+% PREGUNTA: cómo evito el doble resultado? por ejemplo para interseccion([1,1,2,4], [4,2,2,1], I).
+
+interseccion([], _, []).
+interseccion([X|XS], L2, L3) :- member(X, L2), interseccion(XS, L2, I), not(member(X, I)), L3 = [X|I].
+interseccion([X|XS], L2, L3) :- member(X, L2), interseccion(XS, L2, L3), member(X, L3).
+interseccion([X|XS], L2, L3) :- not(member(X, L2)), interseccion(XS, L2, L3).
+
+% ii)
+
+partir(N, L, L1, L2) :- append(L1, L2, L), length(L1, N). % L debe estar instanciado, el resto no es necesario
+
+
+% iii)
+
+borrar([], _, []).
+borrar([X|XS], X, NL) :- borrar(XS, X, NL).
+borrar([Y|XS], X, NL) :- Y \= X, borrar(XS, X, NLR), NL = [Y|NLR].  
+
+
+% iv)
+
+
+sacarDuplicados(L1, L2) :- setof(X, member(X, L1), L2). % PREGUNTA: vale usar setof acá?
+
+
+sacarDuplicados2([], []).
+sacarDuplicados2([X|XS], L2) :- borrar(XS, X, SX), sacarDuplicados2(SX, SR), L2 = [X|SR], !. 
+
+
+% PREGUNTA: quisiera iterar por la lista partiéndola y eliminando de la segunda mitad los elementos de la primera, se puede?
+
+
+% v)
+
+removeElement(X,[X|T],T).
+removeElement(X,[F|T],[F|T1]) :- removeElement(X,T,T1). 
+
+permutacion([],[]).
+permutacion([X|Y],Z) :- permutacion(Y,W), removeElement(X,Z,W).
+
+
+/*
+					perm([1,2,3], Z)
+							|
+							|
+							|
+					perm([2,3], W), remove(1, Z, W)
+							|
+							|
+							|
+					perm([3], W'), remove(2, W, W'), remove(1, Z, W)
+							|
+							|
+							|
+					perm([], W''), remove(3, W', W''), remove(2, W, W'), remove(1, Z, W)
+							|
+							|
+					(W'' <- [])
+							|
+							|
+							|
+					perm([], []), remove(3, W', []), remove(2, W, W'), remove(1, Z, W)
+							|
+							|
+							|
+					(W' <- [3])
+							|
+							|
+							|
+					perm([], []), remove(3, [3], []), remove(2, W, [3]), remove(1, Z, W)
+							|																												|
+						(1)	|																											(2)	|
+							|																												|
+					(W<-[2,3])																										(W<-[3|T])
+							|																												|																				
+							|																												|
+							|																												|
+					perm([], []), remove(3, [3], []), remove(2, [2,3], [3]), remove(1, Z, [2,3])   								perm([], []), remove(3, [3], []), remove(2, XS, []), remove(1, Z, [3|XS])
+							|																												|
+						(1)	|																												|
+							|																												|
+					(Z<-[1,2,3])																									(XS<-[2])
+							|																												|
+							|																												|
+							|																												|
+					perm([], []), remove(3, [3], []), remove(2, [2,3], [3]), remove(1, [1,2,3], [2,3])  						perm([], []), remove(3, [3], []), remove(2, [2], []), remove(1, Z, [3, 2])
+																																			|
+																																		(1)	|
+																																			|
+																																	(Z<-[1,3,2])
+																																			|
+																																			|
+																																			|
+																																perm([], []), remove(3, [3], []), remove(2, [2], []), remove(1, [1,3,2], [3, 2])
+
+Etcétera
+
+*/
+
+
+
+
+
+% PREGUNTA: Qué se puede hacer si L2 está siempre instanciado? No existe la idea de recorrer la lista como en otros paradigmas. No parece trivial hacerlo en O(n).
+
+
+% v)
+
+reparto(L, 1, [L]).
+reparto(L, N, LL) :- N > 1, M is N-1, length(L, LEN), particiones(0, LEN, L, L1, L2), reparto(L2, M, LLR), append([L1], LLR, LL).
+
+
+% vi)
+
+repartoSinVacias([], []).
+repartoSinVacias(L, LL) :- length(L, LEN), particiones(1,LEN, L,L1,L2), repartoSinVacias(L2, LLR), append([L1], LLR, LL).
+
+
+particiones(MIN, MAX, L, L1, L2) :- between(MIN, MAX, I), partir(I, L, L1, L2).
+
+
+
+% Ejercicio 9: TO DO
+
+
+
+% Ejercicio 10 
+
+desde(X,X).
+desde(X,Y) :- N is X+1, desde(N,Y). % Los requerimientos del predicado son desde(+X, -Y). 
+									% Si llega instanciado Y, entra una vez por la primera condición, y luego continúa sumando 1 sin fin.
+									% Si llega X sin estar instanciado, unifica con el primer predicado y luego al intentar obtener N para dar más instanciaciones, no puede resolver el valor de X.
+
+% ii)
+
+desde2(X, X).
+desde2(X, Y) :- nonvar(Y), Y > X.
+desde2(X, Y) :- var(Y), N is X+1, desde(N, Y).
+
+
+
+% Ejercicio 11: TO DO
+
+
+
+% Ejercicio 12
+
+vacio(nil).
+
+raiz(bin(_,V,_), V).
+
+altura(nil, 0).
+altura(bin(I,_,D), H) :- altura(I, HI), altura(D, HD), max(HI, HD, HS), H is HS + 1.
+
+max(A, B, A) :- A >= B, !.
+max(A, B, B) :- A < B.
+
+
+cantidadDeNodos(nil, 0).
+cantidadDeNodos(bin(I,_,D), C) :- cantidadDeNodos(I, CI), cantidadDeNodos(D, CD), C is CI+CD+1.
+
+
+
+% Ejercicio 13
+
+% i)
+
+inorder(nil, []).
+inorder(bin(I,V,D), L) :- inorder(I, LI), inorder(D, LD), append(LI, [V], LIV), append(LIV, LD, L).
+
+% ii) PREGUNTA: hay que dar todos los posibles árboles con esa lista?
+
+arbolConInorder([], nil).
+arbolConInorder(L, AB) :- length(L, LEN), N is LEN-1, between(0, N, I), partir(I, L, LI, LDV), arbolConInorder(LI, AI), LDV = [V|LD], arbolConInorder(LD, AD), AB = bin(AI, V, AD).
+
+
+% iii)
+
+aBB(nil) :- !.
+aBB(bin(nil,_,nil)) :- !.
+aBB(bin(I,V,nil)) :- root(I, R), R < V, aBB(I), !.
+aBB(bin(nil,V,D)) :- root(D, R), R >= V, aBB(D), !.
+aBB(bin(I,V,D)) :- root(I, RI), root(D, RD), RI < V, RD >= V, aBB(I), aBB(D).
+
+
+root(bin(_,V,_), V).
